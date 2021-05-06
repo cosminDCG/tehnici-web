9a68642e079af24dcf7bf6c6a8416957697512b5
Cosmin Dimisca <cosmindimisca@gmail.com>
2021-05-06 09:16:34 +0300

    Add lab 12

--- content-131/lab-10/controller/DogController.js
+++ content-131/lab-10/controller/DogController.js
@@ -20,4 +20,53 @@ router.post("/dogs", (req, res) => {
     res.status(200).send(newDog);
 });
 
+// Read One
+router.get("/dogs/:id", (req, res) => {
+    //ne luam id-ul pe care il cautam
+    let id = req.params.id;
+    let dog = dogService.getDogById(id);
+    console.log(dog);
+
+    if(dog != null && dog != undefined) {
+        res.status(200).send(dog);
+    } else {
+        res.status(404).send("Dog not found!");
+    }
+});
+
+// Update
+router.put("/dogs/:id", (req, res) => {
+    //iau id-ul cainelui pe care vreau sa il actualizez
+    let id = req.params.id;
+    let dog = dogService.updateDog(id, req.body);
+
+    if(dog != null && dog != undefined) {
+        res.status(200).send(dog);
+    } else {
+        res.status(404).send("Dog not found for update!");
+    }
+});
+  
+// Delete
+router.delete("/dogs/:id", (req, res) => {
+    let id = req.params.id;
+    let flag = dogService.deleteDog(id);
+
+    if(flag === true) {
+        res.status(200).send("Dog deleted!");
+    } else {
+        res.status(404).send("Dog not found!");
+    }
+});
+
+router.get("/dogs/filter/property", (req, res) => {
+    let color = req.query.color;
+    let dogs = dogService.getDogsByColor(color);
+    if(dogs.length != 0) {
+        res.status(200).send(dogs);
+    } else {
+        res.status(404).send("No dog found!");
+    }
+});
+
 module.exports = router;
--- content-131/lab-10/service/DogService.js
+++ content-131/lab-10/service/DogService.js
@@ -20,3 +20,69 @@ module.exports.addDog = (newDog) => {
 
   return newDog;
 }
+
+module.exports.getDogById = (id) => {
+  const dogsList = dogRepository.readJSONFile();
+  let foundDog = null
+  dogsList.forEach(dog => {
+      if(dog.id === id) {
+          foundDog = dog;
+      }
+  });
+
+  return foundDog;
+}
+
+module.exports.updateDog = (id, newDog) => {
+  const dogsList = dogRepository.readJSONFile();
+  //iteram prin toti cainii si il cautam dupa id
+  for(let i = 0; i < dogsList.length; i++) {
+      if(dogsList[i].id === id) {
+        //actualizam datele cainelui
+        if(newDog.name) {
+            dogsList[i].name = newDog.name;
+        }
+
+        if(newDog.img) {
+            dogsList[i].img = newDog.img;
+        }
+        
+        
+        //daca am gasit cainele si l-am actualizat, trebuie sa rescriem fisierul db.json
+        dogRepository.writeJSONFile(dogsList);
+        return dogsList[i];
+      }
+  }
+  return null;
+}
+
+module.exports.deleteDog = (id) => {
+  const dogsList = dogRepository.readJSONFile();
+  for(let i = 0; i < dogsList.length; i++) {
+      if(dogsList[i].id === id) {
+          //sterg cainele de pe pozitia i
+          //splice sterge de la indexul i atatea elemente cate indica al doilea argument
+          dogsList.splice(i, 1);
+          dogRepository.writeJSONFile(dogsList)
+          return true;
+      }
+  }
+
+  return false;
+}
+
+module.exports.getDogsByColor = (color) => {
+  const dogsList = dogRepository.readJSONFile();
+  let dogsToReturn = [];
+  if(color == "" || color == null || color == undefined) {
+    return dogsList;
+  }
+
+  for(let i = 0; i < dogsList.length; i++) {
+    if(dogsList[i].colors.includes(color)) {
+      dogsToReturn.push(dogsList[i]);
+    }
+  }
+
+  return dogsToReturn;
+}
